477ca454ae833926d244b4bbaa7c7aac72fc6091
Yasamin Alizadeh <itsYasaminAlizadeh@gmail.com>
2022-01-27 02:04:03 +0330

    Added Header component to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import Home from "./Components/Home";
+import Header from "./Components/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Home />
     </div>
   );
