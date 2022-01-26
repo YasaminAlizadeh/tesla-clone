49bddef23cf0d69a2f11c2bdaa18a9075ac114c3
Yasamin Alizadeh <itsYasaminAlizadeh@gmail.com>
2022-01-27 00:10:01 +0330

    added title and image properties to Section and added more sections

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
 import React from "react";
 import Section from "./Section";
+import { Model3, ModelS, ModelX, ModelY } from "../Assets/Images";
 
 function Home() {
   return (
     <div>
+      <Section title="Model 3" backgroundImg={`url(${Model3})`} />
+      <Section title="Model Y" backgroundImg={`url(${ModelY})`} />
+      <Section title="Model S" backgroundImg={`url(${ModelS})`} />
+      <Section title="Model X" backgroundImg={`url(${ModelX})`} />
       <Section />
     </div>
   );
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,16 +1,18 @@
 import React from "react";
 import styled from "styled-components";
-import { Model3, ModelS, ModelX, ModelY, DownArrow } from "../Assets/Images";
+import { DownArrow } from "../Assets/Images";
 
-function Section() {
+function Section({ title, backgroundImg }) {
   return (
-    <Container>
+    <Container backgroundImg={backgroundImg}>
       <SectionText>
-        <h2>Model 3</h2>
-        <p>Order Online for Touchless Delivery</p>
+        <h2>{title}</h2>
+        <p>
+          Order Online for <a href="#">Touchless Delivery</a>
+        </p>
       </SectionText>
       <ButtonGroup>
-        <LeftButton>Custom Order</LeftButton>
+        <LeftButton tabIndex="0">Custom Order</LeftButton>
         <RightButton>Existing Inventory</RightButton>
       </ButtonGroup>
       <BouncingDownArrow>
@@ -27,9 +29,9 @@ const Container = styled.section`
   flex-direction: column;
   align-items: center;
   justify-content: space-around;
-  gap: 2em;
+  gap: 1.5em;
   height: 100vh;
-  background-image: url(${Model3});
+  background-image: ${(props) => props.backgroundImg};
   background-size: cover;
   background-position: center;
   font-family: var(--font-family);
@@ -45,11 +47,29 @@ const SectionText = styled.div`
     font-weight: 500;
     font-size: clamp(36px, 5vw, 40px);
   }
+
+  p {
+    font-size: 14px;
+
+    a {
+      color: black;
+      text-decoration: none;
+      box-shadow: 0 1px 0px 0px #393c41;
+      transition: box-shadow 0.3s ease-in-out;
+
+      &:hover {
+        box-shadow: 0 1.5px 0px 0px #393c41;
+      }
+    }
+  }
 `;
 
 const ButtonGroup = styled.section`
   display: flex;
-  gap: 2em;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 1em;
 `;
 
 const LeftButton = styled.a`
@@ -60,6 +80,7 @@ const LeftButton = styled.a`
   background-color: #171a20;
   color: white;
   font-size: 12px;
+  font-weight: 500;
   text-transform: uppercase;
   text-align: center;
   border-radius: 32px;
@@ -73,13 +94,34 @@ const RightButton = styled(LeftButton)`
 `;
 
 const BouncingDownArrow = styled.div`
-  height: 30px;
+  height: 45px;
   display: flex;
   align-items: center;
   justify-content: center;
+  margin-bottom: 1.5em;
+  animation: bouncing 2s infinite;
 
   svg {
     height: 100%;
     fill: black;
+  }
+
+  @keyframes bouncing {
+    0%,
+    20%,
+    50%,
+    80%,
+    to {
+      -webkit-transform: translateY(0);
+      transform: translateY(0);
+    }
+    40% {
+      -webkit-transform: translateY(5px);
+      transform: translateY(5px);
+    }
+    60% {
+      -webkit-transform: translateY(3px);
+      transform: translateY(3px);
+    }
   }
 `;
