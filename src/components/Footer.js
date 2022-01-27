a4ed362badfddb2c5161175d10dea6e0882bf879
Yasamin Alizadeh <itsYasaminAlizadeh@gmail.com>
2022-01-28 02:18:17 +0330

    Initialized Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,31 @@
+import React from "react";
+import styled from "styled-components";
+
+function Footer() {
+  return (
+    <Container>
+      <h1>Hello</h1>
+      {links.map((element) => (
+        <a href={element.link} key={`${element.title}_${new Date().getTime()}`}>
+          {element.title}
+        </a>
+      ))}
+    </Container>
+  );
+}
+
+export default Footer;
+
+const Container = styled.div`
+  height: 100vh;
+`;
+
+const links = [
+  { title: "Tesla © 2022", link: "https://www.tesla.com/about" },
+  { title: "Privacy & Legal", link: "https://www.tesla.com/legal" },
+  { title: "Contact", link: "https://www.tesla.com/contact" },
+  { title: "Careers", link: "https://www.tesla.com/careers" },
+  { title: "News", link: "https://www.tesla.com/blog" },
+  { title: "Engage", link: "https://engage.tesla.com/" },
+  { title: "Locations", link: "https://www.tesla.com/findus/list" },
+];
