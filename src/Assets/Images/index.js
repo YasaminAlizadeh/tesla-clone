d9b604687975e15dac8ad72b589c4687ca8cd1dd
Yasamin Alizadeh <itsYasaminAlizadeh@gmail.com>
2022-01-28 01:56:43 +0330

    Added images for sections

--- src/Assets/Images/index.js
+++ src/Assets/Images/index.js
@@ -2,5 +2,8 @@ export const Model3 = require("./model-3.jpg");
 export const ModelS = require("./model-s.jpg");
 export const ModelX = require("./model-x.jpg");
 export const ModelY = require("./model-y.jpg");
+export const SolarPanels = require("./solar-panels.jpg");
+export const SolarRoof = require("./solar-roof.jpg");
+export const Accessories = require("./accessories.jpg");
 export { ReactComponent as DownArrow } from "./down-arrow.svg";
 export { ReactComponent as TeslaLogo } from "./logo.svg";
