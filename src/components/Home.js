d5c881aa244a2b19f2498d183f662c3f377dd255
Yasamin Alizadeh <itsYasaminAlizadeh@gmail.com>
2022-01-28 18:38:39 +0330

    Get the Sections info from array and pass as props

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,99 @@
 import React from "react";
 import Section from "./Section";
-import { Model3, ModelS, ModelX, ModelY } from "../Assets/Images";
+import styled from "styled-components";
+import {
+  Model3,
+  ModelS,
+  ModelX,
+  ModelY,
+  SolarPanels,
+  SolarRoof,
+  Accessories,
+} from "../Assets/Images";
 
 function Home() {
   return (
-    <div>
-      <Section title="Model 3" backgroundImg={`url(${Model3})`} />
-      <Section title="Model Y" backgroundImg={`url(${ModelY})`} />
-      <Section title="Model S" backgroundImg={`url(${ModelS})`} />
-      <Section title="Model X" backgroundImg={`url(${ModelX})`} />
-      <Section />
-    </div>
+    <Container className="container">
+      {sections.map(
+        ({
+          title,
+          description,
+          backgroundImg,
+          leftBtn,
+          rightBtn,
+          bouncingBtn,
+        }) => (
+          <Section
+            title={title}
+            description={description}
+            backgroundImg={`url(${backgroundImg})`}
+            leftBtn={leftBtn}
+            rightBtn={rightBtn}
+            bouncingBtn={bouncingBtn}
+            key={`${title}_${new Date().getTime()}`}
+          />
+        )
+      )}
+    </Container>
   );
 }
 
 export default Home;
+
+const Container = styled.div`
+  max-height: 100vh;
+  overflow-y: scroll;
+  scroll-snap-type: y mandatory;
+`;
+
+// ------------------- Sections info -------------------
+const sections = [
+  {
+    title: "Model 3",
+    description: "Order Online for Touchless Delivery",
+    backgroundImg: Model3,
+    leftBtn: "Custom order",
+    rightBtn: "Existing Inventory",
+    bouncingBtn: true,
+  },
+  {
+    title: "Model Y",
+    description: "Order Online for Touchless Delivery",
+    backgroundImg: ModelY,
+    leftBtn: "Custom order",
+    rightBtn: "Existing Inventory",
+  },
+  {
+    title: "Model S",
+    description: "Order Online for Touchless Delivery",
+    backgroundImg: ModelS,
+    leftBtn: "Custom order",
+    rightBtn: "Existing Inventory",
+  },
+  {
+    title: "Model X",
+    description: "Order Online for Touchless Delivery",
+    backgroundImg: ModelX,
+    leftBtn: "Custom order",
+    rightBtn: "Existing Inventory",
+  },
+  {
+    title: "Solar Panels",
+    description: "Lowest Cost Solar Panels in America",
+    backgroundImg: SolarPanels,
+    leftBtn: "Order now",
+    rightBtn: "Learn more",
+  },
+  {
+    title: "Solar Roof",
+    description: "Produce Clean Energy From Your Roof",
+    backgroundImg: SolarRoof,
+    leftBtn: "Order now",
+    rightBtn: "Learn more",
+  },
+  {
+    title: "Accessories",
+    backgroundImg: Accessories,
+    leftBtn: "Shop now",
+  },
+];
